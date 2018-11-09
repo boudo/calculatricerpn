b0c899fca951f6cc5e82b2464555e87fc0d73e30
Aziz EL ASSRI <aziz.el-assri@ens.uvsq.fr>
2018-11-09 17:30:21 +0100

    Ajout exception concerant operation

---
+++ src/calculatriceRPN/OperandeException.java
@@ -0,0 +1,14 @@
+package calculatriceRPN;
+
+public class OperandeException extends Exception{
+
+	/**
+	 * serialVersionUID = 4960817734138437936L;
+	 */
+	private static final long serialVersionUID = 4960817734138437936L;
+
+	public OperandeException(String message) {
+		super(message);
+	}
+
+}
